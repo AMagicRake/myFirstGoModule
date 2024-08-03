8dc0222c96c160574506209d4f18e79701889f2c
AMagicRake <niel.gow@live.co.uk>
2024-08-03 11:24:03 +0100

    Wrote Test application code for new handler

--- toolkit/tools.go
+++ toolkit/tools.go
@@ -41,6 +41,21 @@ type UploadedFile struct {
 	FileSize         int64
 }
 
+func (t *Tools) UploadOneFIle(r *http.Request, uploadDir string, rename ...bool) (*UploadedFile, error) {
+	renameFile := true
+	if len(rename) > 0 {
+		renameFile = rename[0]
+	}
+
+	files, err := t.UploadFiles(r, uploadDir, renameFile)
+	if err != nil {
+		return nil, err
+	}
+
+	return files[0], nil
+
+}
+
 func (t *Tools) UploadFiles(r *http.Request, uploadDir string, rename ...bool) ([]*UploadedFile, error) {
 	renameFile := true
 	if len(rename) > 0 {
