71521826b36b27593372d74ea6bb43da28aa4b30
AMagicRake <niel.gow@live.co.uk>
2024-08-04 10:26:52 +0100

    improved read JSON error responses

--- toolkit/tools.go
+++ toolkit/tools.go
@@ -213,7 +213,32 @@ func (t *Tools) ReadJSON(w http.ResponseWriter, r *http.Request, data interface{
 
 	err := dec.Decode(data)
 	if err != nil {
-		return err
+		syntaxError := &json.SyntaxError{}
+		unmarshallTypeError := &json.UnmarshalTypeError{}
+		invalidUnmarshallError := &json.InvalidUnmarshalError{}
+
+		switch {
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly-formed JSON")
+		case errors.As(err, &unmarshallTypeError):
+			if unmarshallTypeError.Field != "" {
+				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshallTypeError.Field)
+			}
+			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshallTypeError.Offset)
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+		case strings.HasPrefix(err.Error(), "json: unknown field"):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field")
+			return fmt.Errorf("body contained unknown key %s", fieldName)
+		case err.Error() == "http: request body too large":
+			return fmt.Errorf("body must not be larger than %d bytes", maxBytes)
+		case errors.As(err, &invalidUnmarshallError):
+			return fmt.Errorf("error umnarshalling JSON: %s", err.Error())
+		default:
+			return err
+		}
 	}
 
 	err = dec.Decode(&struct{}{})
