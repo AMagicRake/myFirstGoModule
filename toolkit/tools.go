01b90b741475cdb819fd27dbf09fb83184044764
AMagicRake <niel.gow@live.co.uk>
2024-08-04 11:05:39 +0100

    added Generic JSON Error Module

--- toolkit/tools.go
+++ toolkit/tools.go
@@ -273,3 +273,19 @@ func (t *Tools) WriteJSON(w http.ResponseWriter, status int, data interface{}, h
 	}
 	return nil
 }
+
+// ErrorJSON takes an error and optionally a status code, then generates and sends a JSON error message
+func (t *Tools) ErrorJSON(w http.ResponseWriter, err error, status ...int) error {
+	statusCode := http.StatusBadRequest
+
+	if len(status) != 0 {
+		statusCode = status[0]
+	}
+
+	payload := JSONResponse{
+		Error:   true,
+		Message: err.Error(),
+	}
+
+	return t.WriteJSON(w, statusCode, payload)
+}
--- toolkit/tools_test.go
+++ toolkit/tools_test.go
@@ -2,6 +2,8 @@ package toolkit
 
 import (
 	"bytes"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"image"
 	"image/png"
@@ -297,4 +299,32 @@ func TestTools_WriteJSON(t *testing.T) {
 	if err != nil {
 		t.Errorf("failed to write JSON: %v", err)
 	}
+}
+
+func TestTools_ErrorJSON(t *testing.T) {
+	tool := &Tools{}
+	rr := httptest.NewRecorder()
+	err := tool.ErrorJSON(rr, errors.New("some error"), http.StatusServiceUnavailable)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	payload := JSONResponse{}
+
+	decoder := json.NewDecoder(rr.Body)
+	err = decoder.Decode(&payload)
+
+	if err != nil {
+		t.Error("received error when decoding JSON", err)
+	}
+
+	if !payload.Error {
+		t.Error("error set to false in JSON and it should be true")
+	}
+
+	if rr.Code != http.StatusServiceUnavailable {
+		t.Errorf("wrong status code returned; expected 503, but got %d", rr.Code)
+	}
+
 }
